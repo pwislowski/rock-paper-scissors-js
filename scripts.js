77cd3d293b8aada6c62708a3bfb58fbea87ed89e
Piotr Wislowski <piotr.wislowski@radius.com>
2023-02-08 16:25:35 +0000

    [dev] logic for buttons to be input

--- scripts.js
+++ scripts.js
@@ -8,13 +8,19 @@ function getComputerChoice() {
 }
 
 function getPlayerSelection() {
-    let input = String(prompt("What do you pick? : "));
-
-    return input.toLowerCase();
+    buttons = document.querySelectorAll("button");
+    buttons.forEach((element) => {
+        element.addEventListener(
+            'click',
+            (e) => {
+                let selection = e.target.textContent.toLowerCase();
+                return selection;
+            }
+        )
+    });
 }
 
 function playRound(playerSelection, computerSelection) {
-    console.log(computerSelection);
     if (playerSelection == "rock" && computerSelection == "scissors") {
         return true;
     } else if (playerSelection == "paper" && computerSelection == "rock" ){
@@ -26,31 +32,73 @@ function playRound(playerSelection, computerSelection) {
     } else {
         return false;
     }
-
 }
 
-function game(rounds = 5) {
+function game(rounds = 3) {
     let player = 0;
     let cpu = 0;
 
+    console.log(getPlayerSelection());
+
     for (let i = 0; i < rounds; i++) {
+
         if (playRound(
             getPlayerSelection(),
             getComputerChoice()
         )) {
             player += 1;
+            updateScores('human-score');
         } else {
             cpu += 1;
+            updateScores('cpu-score');
         }
     }
 
     if (player > cpu) {
-        return "You have won!";
+        announceWinner('human');
     }
 
-    return "You have lost...";
-
+    announceWinner('human');
 
 }
 
-console.log(game())
+function updateScores(id) {
+    let score = document.querySelector('#' + id);
+    let currentScore = parseInt(score?.textContent);
+    score.textContent = currentScore + 1;
+
+}
+
+function currentScores(id) {
+    let score = document.querySelector('#' + id);
+
+    return parseInt(score?.textContent)
+}
+
+function announceWinner(winner) {
+    const content = document.querySelector('.content');
+
+    let div = document.createElement('div');
+    
+    if (winner == "human") {
+        div.textContent = "You have won!";
+        div.style.color = 'green';
+    } else {
+        div.textContent = "You have lost...";
+        div.style.color = 'red';
+    };
+    
+    div.style.fontFamily = 'monospace';
+    div.style.textAlign = 'center'
+    div.style.fontSize = '16px'
+    div.style.fontWeight = 'bolder'
+
+
+    content?.appendChild(
+        div
+    );
+}
+
+// ADD FUNCTIONALITY THAT BUTTONS ARE INPUTS
+
+game();
